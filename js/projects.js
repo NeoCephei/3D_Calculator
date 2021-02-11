f98579bfafb466659dbafffe4284040c67304416
NeoCephei <77801754+NeoCephei@users.noreply.github.com>
2021-02-11 17:52:12 +0100

    Basic display

---
+++ js/projects.js
@@ -0,0 +1,24 @@
+var sizes = {
+    itemLength: 0,
+    itemHeight: 0,
+    itemDepth: 0,
+
+    boxLength: 0,
+    boxHeight: 0,
+    boxDepth: 0,
+}
+
+$(() => {
+    $('#deal').on('click', (ev) => {
+        full_Operation();
+    });
+
+});
+
+function full_Operation(){
+    check_Values();
+}
+
+function check_Values(){
+    
+}
