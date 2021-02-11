c0a38bd15088385bdc0517ce1cb92e384307220b
NeoCephei <77801754+NeoCephei@users.noreply.github.com>
2021-02-11 21:10:07 +0100

    Intento de cubo

--- js/projects.js
+++ js/projects.js
@@ -20,7 +20,7 @@ $(() => {
 
 function full_Operation(){
     if (check_Values()){
-        console.log(sizes);
+        console.log(sizes.itemMeasures);
     } else {
         alert('Invalid input');
         reset_Values();
@@ -53,4 +53,73 @@ function reset_Values(){
     $("#box_lenght").val('') 
     $("#box_height").val('') 
     $("#box_depth").val('') 
+}
+
+var canvas = document.createElement("canvas");
+var ctx    = canvas.getContext('2d');
+
+canvas.width  = 800;
+canvas.height = 800;
+
+document.body.appendChild(canvas);
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    // var sizeX = sizes.itemMeasures[0];
+    // var sizeY = sizes.itemMeasures[1];
+    // var sizeZ = sizes.itemMeasures[2]; 
+
+    var sizeX = 15;
+    var sizeY = 4;
+    var sizeZ = 23; 
+    
+    ctx.scale(5, 5);
+    
+    drawCube(50, 50, sizeX, sizeY, sizeZ);
+}
+
+requestAnimationFrame(draw);
+
+
+function drawCube(x, y, wx, wy, h, color) {
+
+    // LINE MODE
+    ctx.lineJoin = "round";
+    
+    // left face
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(x - wx, y - wx * 0.5);
+    ctx.lineTo(x - wx, y - h - wx * 0.5);
+    ctx.lineTo(x, y - h * 1);
+    ctx.closePath();
+    ctx.fillStyle = "#838357"
+    ctx.strokeStyle = "#7a7a51";
+    ctx.stroke();
+    ctx.fill();
+
+    // right face
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(x + wy, y - wy * 0.5);
+    ctx.lineTo(x + wy, y - h - wy * 0.5);
+    ctx.lineTo(x, y - h * 1);
+    ctx.closePath();
+    ctx.fillStyle = "#6f6f49";
+    ctx.strokeStyle = "#676744";
+    ctx.stroke();
+    ctx.fill();
+
+    // center face
+    ctx.beginPath();
+    ctx.moveTo(x, y - h);
+    ctx.lineTo(x - wx, y - h - wx * 0.5);
+    ctx.lineTo(x - wx + wy, y - h - (wx * 0.5 + wy * 0.5));
+    ctx.lineTo(x + wy, y - h - wy * 0.5);
+    ctx.closePath();
+    ctx.fillStyle = "#989865";
+    ctx.strokeStyle = "#8e8e5e";
+    ctx.stroke();
+    ctx.fill();
 }
