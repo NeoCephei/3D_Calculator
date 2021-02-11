0a9f35c3826a2d6558f72097acf5df707ffa2056
NeoCephei <77801754+NeoCephei@users.noreply.github.com>
2021-02-11 18:46:45 +0100

    Check numbers for inputs

--- js/projects.js
+++ js/projects.js
@@ -19,17 +19,31 @@ $(() => {
 });
 
 function full_Operation(){
-    check_Values();
+    if (check_Values()){
+        console.log(sizes);
+    } else {
+        alert('Invalid input');
+        reset_Values();
+    }
 }
 
 function check_Values(){
-    sizes.itemMeasures[0] = $("#item_lenght").val();
-    sizes.itemMeasures[1] = $("#item_height").val();
-    sizes.itemMeasures[2] = $("#item_depth").val();
+    sizes.itemMeasures[0] = Number($("#item_lenght").val());
+    sizes.itemMeasures[1] = Number($("#item_height").val());
+    sizes.itemMeasures[2] = Number($("#item_depth").val());
     
-    sizes.boxMeasures[0] = $("#item_lenght").val();
-    sizes.boxMeasures[1] = $("#item_height").val();
-    sizes.boxMeasures[2] = $("#item_depth").val();
+    sizes.boxMeasures[0] = Number($("#box_lenght").val());
+    sizes.boxMeasures[1] = Number($("#box_height").val());
+    sizes.boxMeasures[2] = Number($("#box_depth").val());
+
+    if (!sizes.itemMeasures.some(isNaN) && !sizes.boxMeasures.some(isNaN)){
+        sizes.itemVolume = sizes.itemMeasures.reduce((acc, current) => acc * current);
+        sizes.boxVolume = sizes.boxMeasures.reduce((acc, current) => acc * current);
+
+        return true;
+    } else {
+        return false;
+    }
 }
 
 function reset_Values(){
