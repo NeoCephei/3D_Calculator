6b8c4ebeb76c53f3f05e3b37bd6b986e20860cb3
NeoCephei <77801754+NeoCephei@users.noreply.github.com>
2021-02-11 22:42:05 +0100

    Added Permutation function

--- js/projects.js
+++ js/projects.js
@@ -123,3 +123,22 @@ function drawCube(x, y, wx, wy, h, color) {
     ctx.stroke();
     ctx.fill();
 }
+
+function perm(xs) {
+    let ret = [];
+
+    for (let i = 0; i < xs.length; i = i + 1) {
+        let rest = perm(xs.slice(0, i).concat(xs.slice(i + 1)));
+
+        if(!rest.length) {
+        ret.push([xs[i]])
+        } else {
+        for(let j = 0; j < rest.length; j = j + 1) {
+            ret.push([xs[i]].concat(rest[j]))
+        }
+        }
+    }
+    return ret;
+}
+
+console.log(perm([1,2,3]).join("\n"));
