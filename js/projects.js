7f94e2d2a3517c471b202732eca5ec8484c70015
NeoCephei <77801754+NeoCephei@users.noreply.github.com>
2021-02-11 18:20:36 +0100

    Getting inputs

--- js/projects.js
+++ js/projects.js
@@ -1,18 +1,21 @@
 var sizes = {
-    itemLength: 0,
-    itemHeight: 0,
-    itemDepth: 0,
+    itemMeasures: [0,0,0],
+    itemVolume: 0,
 
-    boxLength: 0,
-    boxHeight: 0,
-    boxDepth: 0,
+    boxMeasures: [0,0,0],
+    boxVolume: 0,
+
+    boxDeadSpace: 0,
 }
 
 $(() => {
-    $('#deal').on('click', (ev) => {
+    $('#calculate').on('click', (ev) => {
         full_Operation();
     });
 
+    $("#reset").on('click', (ev) =>{
+        reset_Values();
+    });
 });
 
 function full_Operation(){
@@ -20,5 +23,20 @@ function full_Operation(){
 }
 
 function check_Values(){
+    sizes.itemMeasures[0] = $("#item_lenght").val();
+    sizes.itemMeasures[1] = $("#item_height").val();
+    sizes.itemMeasures[2] = $("#item_depth").val();
     
+    sizes.boxMeasures[0] = $("#item_lenght").val();
+    sizes.boxMeasures[1] = $("#item_height").val();
+    sizes.boxMeasures[2] = $("#item_depth").val();
+}
+
+function reset_Values(){
+    $("#item_lenght").val('') 
+    $("#item_height").val('') 
+    $("#item_depth").val('') 
+    $("#box_lenght").val('') 
+    $("#box_height").val('') 
+    $("#box_depth").val('') 
 }
